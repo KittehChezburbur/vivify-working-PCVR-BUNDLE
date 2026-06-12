af3c8af7b29a3228eb252c2b9c2479510b33dbc9
KittehChezburbur <gaynorgreen2026@gmail.com>
2026-06-12 13:32:12 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,14 @@
-#include "main.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
 #include "UnityEngine/AssetBundle.hpp"
-#include "UnityEngine/GameObject.hpp"
-#include "UnityEngine/Renderer.hpp"
-#include "UnityEngine/Material.hpp"
-#include "UnityEngine/Shader.hpp"
 
-// This function finds Quest-specific shaders to replace PCVR ones
-UnityEngine::Shader* GetQuestShader(std::string originalName) {
-    if (originalName.find("Standard") != std::string::npos) {
-        return UnityEngine::Shader::Find(il2cpp_utils::newcsstr("BeatSaber/Oculus/Lite"));
+// Log helper to see why PCVR bundles fail on Chromebook/Quest builds
+void LogBundleInfo(StringW path) {
+    auto bundle = UnityEngine::AssetBundle::LoadFromFile(path);
+    if (!bundle) {
+        // This usually triggers if the PCVR bundle is 'StandaloneWindows64' instead of 'Android'
+        getLogger().error("VIVIFY: Failed to load PCVR bundle from %s. Check platform compatibility!", 
+            to_utf8(csstrtostr(path)).c_str());
+    } else {
+        getLogger().info("VIVIFY: Bundle loaded successfully.");
     }
-    return UnityEngine::Shader::Find(il2cpp_utils::newcsstr("Custom/SimpleLit"));
-}
-
-// Hook into AssetBundle loading
-MAKE_HOOK_MATCH(AssetBundle_LoadFromFile, &UnityEngine::AssetBundle::LoadFromFile, UnityEngine::AssetBundle*, StringW path) {
-    auto* bundle = AssetBundle_LoadFromFile(path);
-    if (bundle) {
-        // Logic to iterate and swap shaders would go here
-        // This prevents the "pink texture" bug on Quest
-    }
-    return bundle;
 }
