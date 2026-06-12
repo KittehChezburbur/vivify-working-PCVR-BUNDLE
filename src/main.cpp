59604880c9164a80538cbdd3da76628b9c36ec67
KittehChezburbur <gaynorgreen2026@gmail.com>
2026-06-12 13:38:46 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
-#include "UnityEngine/AssetBundle.hpp"
+#include "UnityEngine/Shader.hpp"
+#include "UnityEngine/Material.hpp"
 
-// Log helper to see why PCVR bundles fail on Chromebook/Quest builds
-void LogBundleInfo(StringW path) {
-    auto bundle = UnityEngine::AssetBundle::LoadFromFile(path);
-    if (!bundle) {
-        // This usually triggers if the PCVR bundle is 'StandaloneWindows64' instead of 'Android'
-        getLogger().error("VIVIFY: Failed to load PCVR bundle from %s. Check platform compatibility!", 
-            to_utf8(csstrtostr(path)).c_str());
-    } else {
-        getLogger().info("VIVIFY: Bundle loaded successfully.");
+// This replaces PC Shaders with Quest Shaders
+void FixQuestMaterial(UnityEngine::Material* mat) {
+    if (!mat) return;
+    auto shader = mat->get_shader();
+    if (shader) {
+        std::string shaderName = to_utf8(csstrtostr(shader->get_name()));
+        // If the shader is a PC shader, swap it to a Quest-lite version
+        if (shaderName.find("Standard") != std::string::npos) {
+            mat->set_shader(UnityEngine::Shader::Find(il2cpp_utils::newcsstr("BeatSaber/Oculus/Lite")));
+        }
     }
 }
