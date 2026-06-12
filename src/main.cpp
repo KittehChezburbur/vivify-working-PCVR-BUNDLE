9b8baca93a3a687768da4d514b2993064f19b887
KittehChezburbur <gaynorgreen2026@gmail.com>
2026-06-12 13:42:29 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
-#include "UnityEngine/Shader.hpp"
-#include "UnityEngine/Material.hpp"
+#include "UnityEngine/AssetBundle.hpp"
+#include "UnityEngine/AssetBundleCreateRequest.hpp"
 
-// This replaces PC Shaders with Quest Shaders
-void FixQuestMaterial(UnityEngine::Material* mat) {
-    if (!mat) return;
-    auto shader = mat->get_shader();
-    if (shader) {
-        std::string shaderName = to_utf8(csstrtostr(shader->get_name()));
-        // If the shader is a PC shader, swap it to a Quest-lite version
-        if (shaderName.find("Standard") != std::string::npos) {
-            mat->set_shader(UnityEngine::Shader::Find(il2cpp_utils::newcsstr("BeatSaber/Oculus/Lite")));
-        }
+// Function to load the PCVR bundle on Quest
+void LoadPCVRBundle(std::string path) {
+    auto pathStr = il2cpp_utils::newcsstr(path);
+    // On 1.40.8, we use the Async loader for better performance
+    auto req = UnityEngine::AssetBundle::LoadFromFileAsync(pathStr);
+    
+    if (!req) {
+        getLogger().error("Vivify: Could not find the bundle file!");
     }
 }
