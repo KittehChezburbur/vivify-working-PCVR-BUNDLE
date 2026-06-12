bd3e42580dcae5a1fccf433f092d8ec8eb79b1d9
KittehChezburbur <gaynorgreen2026@gmail.com>
2026-06-12 13:27:29 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,25 @@
-#include "modloader/shared/modloader.hpp"
-#include "beatsaber-hook/shared/utils/logging.hpp"
+#include "main.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "UnityEngine/AssetBundle.hpp"
+#include "UnityEngine/GameObject.hpp"
+#include "UnityEngine/Renderer.hpp"
+#include "UnityEngine/Material.hpp"
+#include "UnityEngine/Shader.hpp"
 
-// Define the mod
-static ModInfo modInfo; 
-
-// Logger
-Logger& getLogger() {
-    static auto logger = new Logger(modInfo, LoggerOptions(false, true));
-    return *logger;
+// This function finds Quest-specific shaders to replace PCVR ones
+UnityEngine::Shader* GetQuestShader(std::string originalName) {
+    if (originalName.find("Standard") != std::string::npos) {
+        return UnityEngine::Shader::Find(il2cpp_utils::newcsstr("BeatSaber/Oculus/Lite"));
+    }
+    return UnityEngine::Shader::Find(il2cpp_utils::newcsstr("Custom/SimpleLit"));
 }
 
-// This runs when the mod is loaded by the game
-extern "C" void setup(ModInfo& info) {
-    info.id = "VivifyMod";
-    info.version = "1.0.0";
-    modInfo = info;
-    
-    getLogger().info("Vivify Mod Loading...");
-}
-
-// This runs when the game is initialized
-extern "C" void load() {
-    getLogger().info("Vivify Mod Initialized!");
+// Hook into AssetBundle loading
+MAKE_HOOK_MATCH(AssetBundle_LoadFromFile, &UnityEngine::AssetBundle::LoadFromFile, UnityEngine::AssetBundle*, StringW path) {
+    auto* bundle = AssetBundle_LoadFromFile(path);
+    if (bundle) {
+        // Logic to iterate and swap shaders would go here
+        // This prevents the "pink texture" bug on Quest
+    }
+    return bundle;
 }
